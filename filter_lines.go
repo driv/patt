b9de485bcf1f6c04211e715aea9d7416385b8344
Federico Nafria <federico.nafria@searchspring.com>
2025-05-22 15:39:59 +0200

    Improve reading performance

--- filter_lines.go
+++ filter_lines.go
@@ -7,20 +7,22 @@ import (
 
 func PrintMatchingLines(filter LinesMatcher, reader io.Reader, writer io.Writer) (bool, error) {
 	scanner := bufio.NewScanner(reader)
-	bufferedWriter := bufio.NewWriter(writer)
+
+	buf := make([]byte, 0, 64*1024)
+	scanner.Buffer(buf, 1024*1024)
+
+	bufferedWriter := bufio.NewWriterSize(writer, 256*1024)
 	defer bufferedWriter.Flush()
 
 	match := false
+
 	for scanner.Scan() {
 		line := scanner.Bytes()
 		if filter.Match(line) {
 			match = true
-			_, err := bufferedWriter.Write(line)
-			if err != nil {
-				return false, err
-			}
-			err = bufferedWriter.WriteByte('\n')
-			if err != nil {
+
+			lineWithNewline := append(line, '\n')
+			if _, err := bufferedWriter.Write(lineWithNewline); err != nil {
 				return false, err
 			}
 		}
@@ -29,5 +31,6 @@ func PrintMatchingLines(filter LinesMatcher, reader io.Reader, writer io.Writer)
 	if err := scanner.Err(); err != nil {
 		return false, err
 	}
+
 	return match, nil
 }
